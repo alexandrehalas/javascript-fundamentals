f26f0e20872af0a7eef01e7f93136e1fa92c32d5
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-15 18:04:49 -0300

    Javascript Values and Variables

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is amazing')
+/**
+ * VARIABLES
+ */
+var js = "amazing";
+
+let firstName = "Alexandre";
+const LAST_NAME = "Halas";
+
+let greeting = "Hello";
+let farewell = "Goodbye";
+
+console.log(greeting + " " + firstName + " " + LAST_NAME);
+
+console.log("Javascript is " + js);
+
+firstName = "Elizabete";
+
+console.log(farewell + " " + firstName + " " + LAST_NAME);
