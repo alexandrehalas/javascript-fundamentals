665199f5cd9b7d8c97c06709c256fba3b0e0eab7
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-16 21:26:58 -0300

    Assingment 4 - Basic Operators

--- assignments.js
+++ assignments.js
@@ -66,3 +66,47 @@ console.log("");
 console.log("LECTURE: let, const and var");
 
 //country = "EUA"; //Uncaught TypeError: Assignment to constant variable.
+
+/*
+LECTURE: Basic Operators
+    1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+    2. Increase the population of your country by 1 and log the result to the console
+    3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+    4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+    5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+*/
+
+console.log("");
+console.log("LECTURE: Basic Operators");
+
+const populationSplitInHalf = population / 2;
+console.log("Population in each half: " + populationSplitInHalf);
+
+population += 1;
+console.log("population: " + population);
+
+const finlandPopulation = 6000000;
+console.log(
+  "My country has more people than Finland? ",
+  population > finlandPopulation
+);
+
+console.log(
+  "My country has less people than 33 million people? ",
+  33000000 > population
+);
+
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " people speak " +
+  language;
+console.log(description);
