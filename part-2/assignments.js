e00b4f74b1fd9e54964c3f5903416acc184370c4
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-11 09:47:15 -0300

    Assingment 20 - LECTURE: Object Methods

--- part-2/assignments.js
+++ part-2/assignments.js
@@ -204,7 +204,7 @@ console.log("LECTURE: Introduction to Objects");
 const myCountry = {
   country: "Brazil",
   capital: "Brasilia",
-  language: "Portuguese",
+  language: "portuguese",
   population: 214,
   neighbours: [
     "Uruguay",
@@ -218,6 +218,13 @@ const myCountry = {
     "Suriname",
     "French Guiana",
   ],
+  describe: function () {
+    return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`;
+  },
+  checkIsland: function () {
+    this.isIsland = this.neighbours.length === 0;
+    return this.isIsland;
+  },
 };
 
 console.log(myCountry);
@@ -243,3 +250,22 @@ console.log(myCountry);
 
 myCountry["population"] -= 2;
 console.log(myCountry);
+
+/*
+LECTURE: Object Methods
+  1. Add a method called 'describe' to the 'myCountry' object. This method
+will log a string to the console, similar to the string logged in the previous
+assignment, but this time using the 'this' keyword.
+  2. Call the 'describe' method
+  3. Add a method called 'checkIsland' to the 'myCountry' object. This
+method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and false if
+there are. Use the ternary operator to set the property.
+*/
+
+console.log("");
+console.log("LECTURE: Object Methods");
+
+console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
+console.log(myCountry.isIsland);
